f6553fe4493adf81ea4d030bccd8964a08d7e118
hammad084 <hraservices123@gmail.com>
2025-05-19 22:26:44 +0500

    changes

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -27,7 +27,8 @@ const Header = () => {
         try {
             const response = await axios.post('https://job-platform.up.railway.app/candidate/candidateLogout');
             if (response.status === 200) {
-                toast.success("Candidate Loggedout successfully!")
+                toast.success("Candidate Loggedout successfully!");
+                setTimeout(() => window.location.reload(), 10000); 
             }
         } catch (error) {
 
@@ -38,7 +39,8 @@ const Header = () => {
         try {
             const response = await axios.post('https://job-platform.up.railway.app/employee/employeeLogout');
             if (response.status === 200) {
-                toast.success("Employee Loggedout successfully!")
+                toast.success("Employee Loggedout successfully!");
+                setTimeout(() => window.location.reload(), 10000); 
             }
         } catch (error) {
 
@@ -49,7 +51,8 @@ const Header = () => {
         try {
             const response = await axios.post('https://job-platform.up.railway.app/admin/adminLogout');
             if (response.status === 200) {
-                toast.success("Admin Loggedout successfully!")
+                toast.success("Admin Loggedout successfully!");
+                setTimeout(() => window.location.reload(), 10000); 
             }
         } catch (error) {
 
